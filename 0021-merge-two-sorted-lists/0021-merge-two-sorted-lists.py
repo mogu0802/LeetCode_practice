8b1c8f9d9f85ed197320136d8dac07e82e5478e2
mogu0802 <bzp5d72nmg@privaterelay.appleid.com>
2026-06-07 00:00:24 +0800

    Time: 0 ms (100%), Space: 19.3 MB (33.02%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,14 +7,14 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         first = ListNode(0)
         num = first
-        while list1 and list2:
-            if list1.val < list2.val:
+        while list1 and list2:                  ###如果其中一個還有東西
+            if list1.val < list2.val:           ###誰小存誰的頭
                 num.next = list1
                 list1 = list1.next
             else:
                 num.next = list2
                 list2 = list2.next
-            num = num.next
-        num.next = list1 if list1 else list2
+            num = num.next                      ###移動到下一個儲存格
+        num.next = list1 if list1 else list2    ###看誰的還有剩全部排進去
         return first.next
         
